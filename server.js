a934468dfb54b5265e7398ecd3f96cd5e9e3d5a7
Olga Chevdarova <olgachevdarova@gmail.com>
2018-06-17 14:19:15 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-const {createServer} = require('http');
 const express = require('express');
-const compression = require('compression');
-const morgan = require('morgan');
+const bodyParser = require('body-parser')
 const path = require('path');
-
-
-const normalizePort = port => parseInt(port, 10);
-const PORT = normalizePort(process.env.PORT || 5000);
-
 const app = express();
-const dev = app.get('env') !== 'production';
+app.use(express.static(path.join(__dirname, 'build')));
 
-if(!dev) {
-    app.disable('x-powered-by');
-    app.use(compression());
-    app.use(morgan('common'));
-
-    app.use(express.static(path.resolve(__dirname, 'build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    })
-}
-
-if(dev) {
-    app.use(morgan('dev'))
-}
-
-const server = createServer(app);
-server.listen(PORT, err => {
-    if(err) throw err;
-    console.log('Started');
+app.get('/ping', function (req, res) {
+    return res.send("<h1>Главная страница</h1>");
 });
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get("/", function(request, response){
+
+    response.send("<h1>Главная страница</h1>");
+});
+
+app.listen(process.env.PORT || 8080);
